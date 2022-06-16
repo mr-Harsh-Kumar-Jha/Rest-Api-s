6fa3a1fa0ea5f27f84187b2d8f3a824b30d66b42
adarsh-shahi <adarshshahi1009@gmail.com>
2022-06-16 06:22:27 +0530

    add password hashing in register route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
 const router = new express.Router()
 const User = require('../models/user')
 
@@ -7,11 +8,12 @@ router.post('/register', async (req, res) => {
     const user = new User(req.body);
 
     try{
+        user.password = await bcrypt.hash(user.password, 8);
         await user.save();
         res.status(201).send(user);
     }
     catch(e){
-        res.status(400).send();
+        res.status(400).send(e);
     }
 
 
